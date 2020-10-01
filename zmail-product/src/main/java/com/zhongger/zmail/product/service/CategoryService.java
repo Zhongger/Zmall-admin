b47fda3370d49ec6e25f26a64de5f965303b8f54
Zhongger <973593026@qq.com>
2020-10-01 20:35:56 +0800

    fix:CategoryService 多写了一个L

--- zmail-product/src/main/java/com/zhongger/zmail/product/service/CategoryService.java
+++ zmail-product/src/main/java/com/zhongger/zmail/product/service/CategoryService.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.zhongger.zmail.common.utils.PageUtils;
 import com.zhongger.zmail.product.entity.CategoryEntity;
 
-import java.util.List;
+
 import java.util.Map;
 
 /**
@@ -18,6 +18,6 @@ public interface CategoryService extends IService<CategoryEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
 
-    L
+
 }
 
