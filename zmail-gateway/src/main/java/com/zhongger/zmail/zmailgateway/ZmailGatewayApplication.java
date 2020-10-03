0e7d15d503e247992e6456994ca46f74627c018e
Zhongger <973593026@qq.com>
2020-10-03 23:35:09 +0800

    feat：添加GateWay网关

---
+++ zmail-gateway/src/main/java/com/zhongger/zmail/zmailgateway/ZmailGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zhongger.zmail.zmailgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmailGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmailGatewayApplication.class, args);
+	}
+
+}
---
+++ zmail-gateway/src/test/java/com/zhongger/zmail/zmailgateway/ZmailGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhongger.zmail.zmailgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmailGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
