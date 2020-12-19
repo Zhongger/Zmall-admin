2f4938bc41ebc43a53006559e944a7cb5454808a
Zhongger <973593026@qq.com>
2020-12-19 21:25:43 +0800

    feat:加入MyBatis分页插件；前端项目添加进来

---
+++ zmail-product/src/main/java/com/zhongger/zmail/product/config/MyBatisConfig.java
@@ -0,0 +1,16 @@
+package com.zhongger.zmail.product.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setOverflow(true);
+        paginationInterceptor.setLimit(1000);
+        return paginationInterceptor;
+    }
+}
