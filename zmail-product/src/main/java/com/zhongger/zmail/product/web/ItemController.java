f7bd6c03b137fb3cbcfed06e84bd60ac96a08300
Zhongger <973593026@qq.com>
2021-02-20 10:48:12 +0800

    feat:商品详情页面环境搭建

---
+++ zmail-product/src/main/java/com/zhongger/zmail/product/web/ItemController.java
@@ -0,0 +1,13 @@
+package com.zhongger.zmail.product.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemController {
+
+    @GetMapping("/{skuId}.html")
+    public String skuItem(Long skuId){
+        return "item";
+    }
+}
